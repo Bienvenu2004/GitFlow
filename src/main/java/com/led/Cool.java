3785a7820b48704a47c77f31d6e3a0382ed44f99
Ambassa Bienvenu <kidsglory1@gmail.com>
2024-12-03 04:25:19 +0100

    all done

---
+++ src/main/java/com/led/Cool.java
@@ -0,0 +1,7 @@
+package com.led;
+
+public class Cool {
+    public Cool(){
+        System.out.println("Cool");
+    }
+}
---
+++ src/main/java/com/led/Main.java
@@ -0,0 +1,10 @@
+package com.led;
+
+public class Main {
+    public static void main(String[] args) {
+        Cool cool = new Cool();
+        System.out.println("Hello, World!"
+
+        );
+    }
+}
